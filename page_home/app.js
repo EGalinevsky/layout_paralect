e0e793abfd742ac1d9378fb40478cb809e4ba088
EGalinevsky <gluk40k@gmail.com>
2021-07-08 10:55:53 +0300

    refactoring contacts

--- page_home/app.js
+++ page_home/app.js
@@ -1,12 +1,14 @@
-let btnContainer = document.getElementById("nav");
+// let btnContainer = document.getElementById("nav");
 
-let  btns = btnContainer.getElementsByClassName("nav__link");
+// let  btns = btnContainer.getElementsByClassName("nav__link");
 
 
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-      var current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
-    });
-  } 
+// for (var i = 0; i < btns.length; i++) {
+//     btns[i].addEventListener("click", function() {
+//       let current = document.getElementsByClassName("active");
+//       if(current.length > 0){
+//         current[0].className = current[0].className.repeat(" active", "")
+//       }
+//       this.className += " active"
+//     });
+//   } 
